efd8f966f6767960df9088d2404dfb2ea25646f9
jbbok <qkrwjd94@gmail.com>
2024-09-25 16:59:45 +0900

    240925 삼항연산자

--- 240925/script.js
+++ 240925/script.js
@@ -63,7 +63,11 @@
 // (삼항연산자로 풀기!)
 // 조건 ? true일때 행위 : false일때 행위;
 
-let a = Number(prompt("숫자를 입력해주세요."));
-let b = Number(prompt("숫자를 입력해주세요."));
+// let a = Number(prompt("숫자를 입력해주세요."));
+// let b = Number(prompt("숫자를 입력해주세요."));
 
-a > b ? alert("a가 b보다 큽니다") : alert("a가 b보다 작습니다");
+// a > b ? alert("a가 b보다 큽니다") : alert("a가 b보다 작습니다");
+
+const a = Number(prompt("첫번째 숫자를 입력해주세요", 10));
+const b = Number(prompt("두번째 숫자를 입력해주세요", 3));
+alert(`${a}이(가) ${b}보다 ${a > b ? "큽니다" : "작습니다"}`);
